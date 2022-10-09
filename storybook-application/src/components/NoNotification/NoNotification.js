536f89f6c4a19c037fef629bc3da5a85442c9026
Premnath <rexprem1991@gmail.com>
2022-10-09 21:20:02 +0530

    icon added

--- storybook-application/src/components/NoNotification/NoNotification.js
+++ storybook-application/src/components/NoNotification/NoNotification.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './NoNotification.css'
+import Icons from './icons'
 
 function NoNotification(props){
     const {size = 'desktop',...rest} = props
     return (
         <div className={`div ${size}`}{...rest}>
-
+            <image />         
         </div>
     )
 }
